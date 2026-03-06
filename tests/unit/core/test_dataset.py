8524680fd1660938a70a196497ad7045a3dfcf15
gmartinon-ed <gregoire.martinon@emerton-data.com>
2026-03-06 14:27:48 +0100

    feat: tests for dataset and records property

--- tests/unit/core/test_dataset.py
+++ tests/unit/core/test_dataset.py
@@ -1,8 +1,23 @@
 from glide.core.dataset import Dataset
 
 
-def test_dataset_constructor():
+def test_dataset_empty():
+    dataset = Dataset()
+    assert dataset == []
+
+
+def test_dataset_iadd():
     dataset = Dataset()
     dataset += [{"a": 0}]
+    assert dataset == [{"a": 0}]
+
+
+def test_dataset_imul():
+    dataset = Dataset([{"a": 0}])
     dataset *= 2
     assert dataset == [{"a": 0}, {"a": 0}]
+
+
+def test_dataset_records():
+    dataset = Dataset([{"a": 0}, {"b": 1}])
+    assert dataset.records == [{"a": 0}, {"b": 1}]
