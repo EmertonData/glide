eccbb93777c50951d44a11911bf01b005ffee65d
imerad <26345753+imerad@users.noreply.github.com>
2026-03-17 11:22:48 +0100

    Classical mean estimator (#24)
    * Implemented ClassicalMeanEstimator
    
    * Implemented ClassicalMeanEstimator
    
    * addressed first review

--- glide/core/utils.py
+++ glide/core/utils.py
@@ -2,8 +2,8 @@ import numpy as np
 
 
 def compute_effective_sample_size(y_true: np.ndarray, effective_std: float) -> int:
-    n = len(y_true)
-    var_y_true = np.var(y_true, ddof=1)
+    n = np.sum(~np.isnan(y_true))
+    var_y_true = np.nanvar(y_true, ddof=1)
     std_of_mean = np.sqrt(var_y_true / n)
     effective_sample_size = int(n * (std_of_mean / effective_std) ** 2)
     return effective_sample_size
