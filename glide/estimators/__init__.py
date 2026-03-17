63a4430e8661f5b9c2757dc05baef77906bd6025
imerad <26345753+imerad@users.noreply.github.com>
2026-03-17 14:35:18 +0100

    feat:new InferenceResult objects applied precommit

--- glide/estimators/__init__.py
+++ glide/estimators/__init__.py
@@ -1,5 +1,5 @@
-from glide.estimators.classical import ClassicalMeanEstimator
 from glide.estimators.asi import ASIMeanEstimator
+from glide.estimators.classical import ClassicalMeanEstimator
 from glide.estimators.ppi import PPIMeanEstimator
 
 __all__ = ["ClassicalMeanEstimator", "PPIMeanEstimator", "ASIMeanEstimator"]
