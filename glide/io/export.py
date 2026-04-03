c2148289b2472fae5858ada2e92910bbd652b3d5
imerad <26345753+imerad@users.noreply.github.com>
2026-04-03 14:04:51 +0200

    added doctest to to_json

--- glide/io/export.py
+++ glide/io/export.py
@@ -16,6 +16,30 @@ def to_json(result: MeanInferenceResult) -> str:
     -------
     str
         A JSON-formatted string representation of the inference result with 2-space indentation.
+
+    Examples
+    --------
+    >>> from glide.core.dataset import Dataset
+    >>> from glide.estimators import ClassicalMeanEstimator
+    >>> from glide.io import to_json
+    >>> dataset = Dataset([{"y_true":i} for i in range(10)])
+    >>> estimator = ClassicalMeanEstimator()
+    >>> estimation_result = estimator.estimate(dataset, y_field="y_true", confidence_level=0.9)
+    >>> print(to_json(estimation_result))
+    {
+      "confidence_interval": {
+        "mean": 4.5,
+        "std": 0.9574271077563381,
+        "confidence_level": 0.9,
+        "lower_bound": 2.9251725492653295,
+        "upper_bound": 6.07482745073467
+      },
+      "metric_name": "Metric",
+      "estimator_name": "ClassicalMeanEstimator",
+      "n": 10,
+      "mean": 4.5,
+      "std": 0.9574271077563381
+    }
     """
     data = asdict(result)
     data["mean"] = result.mean
