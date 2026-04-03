276d9fc1b7cea00fd5db71cddc2db215e4057b0b
imerad <26345753+imerad@users.noreply.github.com>
2026-04-03 15:29:28 +0200

    Add doctest to to_json (#74)
    * added doctest to to_json
    
    * updated changelog
    
    * fixed issue with varying number of decimals
    
    * addressed review

--- glide/io/export.py
+++ glide/io/export.py
@@ -16,6 +16,29 @@ def to_json(result: MeanInferenceResult) -> str:
     -------
     str
         A JSON-formatted string representation of the inference result with 2-space indentation.
+
+    Examples
+    --------
+    >>> from glide.io import to_json
+    >>> from glide.core.mean_inference_result import MeanInferenceResult
+    >>> from glide.core.clt_confidence_interval import CLTConfidenceInterval
+    >>> confidence_interval = CLTConfidenceInterval(mean=0, std=1)
+    >>> inference_result = MeanInferenceResult(confidence_interval=confidence_interval, \
+    metric_name="metric", estimator_name="none")
+    >>> print(to_json(inference_result))  # doctest: +ELLIPSIS
+    {
+      "confidence_interval": {
+        "mean": 0,
+        "std": 1,
+        "confidence_level": 0.95,
+        "lower_bound": -1.959963984540054,
+        "upper_bound": 1.959963984540054
+      },
+      "metric_name": "metric",
+      "estimator_name": "none",
+      "mean": 0,
+      "std": 1
+    }
     """
     data = asdict(result)
     data["mean"] = result.mean
