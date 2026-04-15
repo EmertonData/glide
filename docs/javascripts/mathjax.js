ba786b828ff9f2f0c1f70e1661927c8de43e7e5b
imerad <26345753+imerad@users.noreply.github.com>
2026-04-15 18:09:57 +0200

    fixed latex

--- docs/javascripts/mathjax.js
+++ docs/javascripts/mathjax.js
@@ -3,5 +3,13 @@ window.MathJax = {
     inlineMath: [["\\(", "\\)"], ["$", "$"]],
     displayMath: [["\\[", "\\]"], ["$$", "$$"]],
     processEscapes: true,
+    processEnvironments: true
   }
 };
+
+document$.subscribe(() => {
+  MathJax.startup.output.clearCache();
+  MathJax.typesetClear();
+  MathJax.texReset();
+  MathJax.typesetPromise();
+});
