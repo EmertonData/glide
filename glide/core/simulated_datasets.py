9258d9e31a4fe888cfddbed723a41c1cbdb0b403
Mohammed Raki <mohammed.raki@emerton-data.com>
2026-04-20 10:31:24 +0200

    review comments

--- glide/core/simulated_datasets.py
+++ glide/core/simulated_datasets.py
@@ -451,9 +451,9 @@ def generate_binary_dataset_with_oracle_sampling(
     4
     >>> len(uncertainty)
     4
-    >>> bool(np.isin(y_true, [0.0, 1.0]).all())
+    >>> bool(np.all(np.isin(y_true, [0.0, 1.0])))
     True
-    >>> bool(np.isin(y_proxy, [0.0, 1.0]).all())
+    >>> bool(np.all(np.isin(y_proxy, [0.0, 1.0])))
     True
     >>> bool(np.all((uncertainty >= 0) & (uncertainty <= 1)))
     True
